14d22cf27f44a15d0894f5536ad013e72a74fe0e
Abdul Ahad <ahad.devs@gmail.com>
2026-09-24 20:46:35 +0500

    Added service for google sheet url validation and extracting spreadsheet id

---
+++ application/services/add_google_sheet.py
@@ -0,0 +1,42 @@
+from urllib.parse import urlparse
+
+class AddGoogleSheet:
+    def execute(self, sheet_url: str):
+        # Validate if googel sheet url is not empty or missing
+        if not sheet_url:
+            raise ValueError("Google Sheet URL cannot be empty.")
+
+        parsed_url = urlparse(sheet_url)
+
+        # Validate HTTPs in the URL
+        if parsed_url.scheme != "https":
+            raise ValueError("URL has to contain https.")
+
+        # Validate if the right domain is triggered i.e google docs
+        if parsed_url.netloc != "docs.google.com":
+            raise ValueError("Invalid Google Sheet URL.")
+
+        # Validate if the google spreadsheet id is missing
+        if "/spreadsheets/d/" not in parsed_url.path:
+            raise ValueError("Invalid Google Sheet URL.")
+
+        spreadsheet_id = self.extract_spreadsheet_id(parsed_url.path)
+
+        return spreadsheet_id
+
+    # Extracting spreadsheet id for later APIs
+    def extract_spreadsheet_id(self, path: str) -> str:
+        split_path = path.split("/")
+
+        # Find the index d and id is the next item in split_path list
+        try:
+            spreadsheet_index = split_path.index("d")
+            spreadsheet_id = split_path[spreadsheet_index + 1]
+        
+        except(ValueError, IndexError):
+            raise ValueError("Could not extract spreadsheet ID")
+
+        if not spreadsheet_id:
+            raise ValueError("Spreadsheet ID cannot be empty")
+
+        return spreadsheet_id
