561fc8138a545519ce4c477ddd998dcbafebb90b
Abdul Ahad <ahad.devs@gmail.com>
2026-09-25 18:38:30 +0500

    Modified add_google_sheet service for saving whole path instead of id

--- application/services/add_google_sheet.py
+++ application/services/add_google_sheet.py
@@ -1,6 +1,10 @@
 from urllib.parse import urlparse
+from application.repositories.google_sheet_repository import GoogleSheetRepository
 
 class AddGoogleSheet:
+    def __init__(self, repository: GoogleSheetRepository):
+        self.repository = repository
+
     def execute(self, sheet_url: str):
         # Validate if googel sheet url is not empty or missing
         if not sheet_url:
@@ -20,23 +24,26 @@ class AddGoogleSheet:
         if "/spreadsheets/d/" not in parsed_url.path:
             raise ValueError("Invalid Google Sheet URL.")
 
-        spreadsheet_id = self._extract_spreadsheet_id(parsed_url.path)
+        # spreadsheet_id = self._extract_spreadsheet_id(parsed_url.path)
 
-        return spreadsheet_id
-
-    # Extracting spreadsheet id for later APIs
-    def _extract_spreadsheet_id(self, path: str) -> str:
-        split_path = path.split("/")
-
-        # Find the index d and id is the next item in split_path list
-        try:
-            spreadsheet_index = split_path.index("d")
-            spreadsheet_id = split_path[spreadsheet_index + 1]
+        self.repository.save(sheet_url)
         
-        except(ValueError, IndexError):
-            raise ValueError("Could not extract spreadsheet ID")
+        return sheet_url
 
-        if not spreadsheet_id:
-            raise ValueError("Spreadsheet ID cannot be empty")
+    # Removed after making google_sheet_repository, saving path instead of just id.
+    # Extracting spreadsheet id for later APIs
+    # def _extract_spreadsheet_id(self, path: str) -> str:
+    #     split_path = path.split("/")
 
-        return spreadsheet_id
+    #     # Find the index d and id is the next item in split_path list
+    #     try:
+    #         spreadsheet_index = split_path.index("d")
+    #         spreadsheet_id = split_path[spreadsheet_index + 1]
+        
+    #     except(ValueError, IndexError):
+    #         raise ValueError("Could not extract spreadsheet ID")
+
+    #     if not spreadsheet_id:
+    #         raise ValueError("Spreadsheet ID cannot be empty")
+
+    #     return spreadsheet_id
